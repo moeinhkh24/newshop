3e7d2c856f82f8fce90b6b6d6d5264a22f6484e1
moeinhkh24 <moeinnmt66@gmail.com>
2025-04-18 15:37:38 +0330

    update templates in home

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.views import generic
 app_name ='home'
 
 urlpatterns = [
-    path('',views.HomeView.as_view(),name='home')
+    path('',views.HomeView.as_view(),name='home'),
+    path('detail/',generic.TemplateView.as_view(template_name='home/product-details.html'),name='detail')
 ]
 
