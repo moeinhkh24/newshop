284ac533fc7bc5b9291e80d6f2f11dabff04ef3e
moeinhkh24 <moeinnmt66@gmail.com>
2025-04-21 20:20:51 +0330

    update

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 
 app_name ='home'
 
 urlpatterns = [
     path('',views.HomeView.as_view(),name='home'),
     path('product/<int:pk>/',views.ProductDetailView.as_view(),name='product_detail'),
-    path('comment/<int:pk>/',views.CommentCreateView.as_view(),name='comment_create')
+    path('comment/<int:pk>/',views.CommentCreateView.as_view(),name='comment_create'),
+    path('cart/',TemplateView.as_view(template_name = 'home/cart.html'),name ='cart')
 ]
 
