316ec2d5bc4150f81eb4a89b3a52daf81680941d
moeinhkh24 <moeinnmt66@gmail.com>
2025-04-22 14:43:03 +0330

    uodate

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from .cart import Cart
+from .forms import AddToCartForm
+from home.models import Product
 
 
+def CartView(View):
+    def get(self,request):
+        cart = Cart(request)
+        for item in cart:
+            item['quantity_update_form'] = AddToCartForm(initial={
+                'quantity':item['quantity'],
+                'inplace':True  
+            })
+        return render(request,'shop/cart.html',{'cart':cart})
 
-def test_view(request):
+def CartAddView(View):
+    pass
+
+def CartRemoveView(View):
+    pass
+
+def CartClearView(View):
     pass
